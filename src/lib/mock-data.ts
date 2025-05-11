04a7212f3be884496b2631a8a250b5eaf507087f
alexischebeste <alexispaulino.chebeste@estudiantes.unahur.edu.ar>
2025-05-10 21:04:21 -0300

    hooks and mock type vehicle

---
+++ src/lib/mock-data.ts
@@ -0,0 +1,29 @@
+import { VehicleType } from "../types";
+
+export const vehicleTypes: VehicleType[] = [
+  {
+    _id: "vtype1",
+    name: "Camión Articulado",
+    description: "Camión con remolque articulado",
+  },
+  {
+    _id: "vtype2",
+    name: "Camión Rígido",
+    description: "Camión sin remolque",
+  },
+  {
+    _id: "vtype3",
+    name: "Furgón",
+    description: "Vehículo cerrado para transporte de mercancías",
+  },
+  {
+    _id: "vtype4",
+    name: "Camioneta",
+    description: "Vehículo ligero para transporte de mercancías",
+  },
+  {
+    _id: "vtype5",
+    name: "Tractor con Semirremolque",
+    description: "Combinación de tractor y semirremolque",
+  },
+]

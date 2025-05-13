eac44d40f2c5000724159e2185cfe24b3002591f
elielizabeth <patriciaelizabeth.albornoz@estudiantes.unahur.edu.ar>
2025-05-12 21:11:12 -0300

    act routes

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import TypeVehicle from "./pages/TypeVehicle";
-import VehiclePage from "./pages/VehiclePage";
+//import VehiclePage from "./pages/VehiclePage";
+import DriverCreate from "./pages/driver/driverCreate"; //para probar
+import DriverPage from "./pages/driver/driverPage";
 import Layout from "./components/Layout";
 
 export function AppRouter() {
@@ -10,7 +12,9 @@ export function AppRouter() {
       <Route element={<Layout />}>
         <Route path='/' element={<Dashboard />} />
         <Route path='/type-vehicle' element={<TypeVehicle />} />
-        <Route path="/vehicles" element={<VehiclePage />} />
+        {/*<Route path="/vehicles" element={<VehiclePage />} />*/}
+        <Route path='/drivers' element={<DriverPage />} />
+        <Route path='/driver-create' element={<DriverCreate />} />
       </Route>
     </Routes>
   );

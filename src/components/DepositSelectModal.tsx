32d55c77b63cd1f3699cfbefc652095978de1cc5
alexischebeste <alexispaulino.chebeste@estudiantes.unahur.edu.ar>
2025-06-18 00:41:47 -0300

    feat: base search depots

--- src/components/DepositSelectModal.tsx
+++ src/components/DepositSelectModal.tsx
@@ -1,6 +1,7 @@
-import { Dialog, DialogTitle, DialogContent, Card, CardContent, Typography, Grid, Box, Chip, Button } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, Card, CardContent, Typography, Grid, Box, Chip, Button, TextField } from "@mui/material";
 import { DepositoDto } from "../api/generated";
-import { Building2, Clock, MapPin, X } from "lucide-react";
+import { Building2, Clock, MapPin, Search, X } from "lucide-react";
+import { useState } from "react";
 
 interface DepositoSelectModalProps {
   open: boolean;
@@ -19,9 +20,20 @@ export const DepositoSelectModal = ({
   onSelect,
   title = "Seleccionar depósito",
 }: DepositoSelectModalProps) => {
+
+  const [filteredDepots, setFilteredDepots] = useState<DepositoDto[]>(depots);
+
+  const handleSearch = (searchTerm: string) => {
+    const lowerCaseTerm = searchTerm.toLowerCase();
+    const filtered = depots.filter((dep) =>
+      dep.nombre.toLowerCase().includes(lowerCaseTerm)
+    );
+    setFilteredDepots(filtered);
+  };
+
   return (
     <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth 
-      sx={{ '& .MuiDialog-paper': { width: '80%', maxHeight: 600 } }}
+      sx={{ '& .MuiDialog-paper': { width: '80%', minHeight:600 ,maxHeight: 600 } }}
     >
       <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center"}}>
 
@@ -37,9 +49,49 @@ export const DepositoSelectModal = ({
           <X className="size-5 text-gray-500 hover:text-gray-700" onClick={onClose} />
         </Button>
       </Box>
-      <DialogContent >
+      <DialogContent sx={{display: "flex", flexDirection: "column", gap: 2}}>
+        {/* <TextField
+          variant="outlined"
+          placeholder="Buscar depósito..."
+          fullWidth
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <MapPin className="h-5 w-5 text-gray-500" />
+            ),
+          }}
+          sx={{ marginBottom: 2 }}
+          onChange={(e) => {
+            const searchTerm = e.target.value.toLowerCase();
+            const filteredDepots = depots.filter((dep) =>
+              dep.nombre.toLowerCase().includes(searchTerm)
+            );
+            onSelect(filteredDepots.length > 0 ? filteredDepots[0]._id : "");
+          }}
+
+        >
+        </TextField> */}
+        <TextField
+          variant="outlined"
+          placeholder="Buscar depósito..."
+          fullWidth
+          size="small"
+          className="inside-paper"
+          InputProps={{
+            startAdornment: (
+              <Search className="h-5 w-5 text-gray-500" />
+            ),
+          }}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+            console.log("Filtered Depots: ", filteredDepots);
+          }}
+
+        >
+        </TextField>
+
         <Grid container spacing={2} >
-          {depots.map((dep) => (
+          {filteredDepots.map((dep) => (
             <Grid item xs={12}  key={dep._id}>
               <Card
                 onClick={() => {
